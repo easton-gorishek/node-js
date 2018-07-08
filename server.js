b64d2be9b046a7b4603656e7c6b58cbc3c6e765f
Easton Gorishek <ieaston26@gmail.com>
2018-07-08 15:34:50 -0700

    used console.log to see async in action

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
+// const http = require('http');
+
+// http.createServer(function(request, response) {
+//     response.writeHead(200, { 'Content-Type': 'text/plain' });
+//     response.write('Hello World!');
+//     response.end();
+// }).listen(8888);
+
 const http = require('http');
 
-http.createServer(function(request, response) {
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.write('Hello World!');
-    response.end();
-}).listen(8888);
+function onRequest(req, res) {
+    console.log('request received.');
+    res.writeHead(200, { 'Content-Type': 'text/plan' });
+    res.write('Hello World!');
+    res.end();
+}
+
+http.createServer(onRequest).listen(8888);
+
+console.log('Server has started');
