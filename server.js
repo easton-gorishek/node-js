d11102b2b2c6b385c90e113b535623144957a746
Easton Gorishek <ieaston26@gmail.com>
2018-07-08 15:42:27 -0700

    required server module in index.js

--- server.js
+++ server.js
@@ -8,13 +8,16 @@
 
 const http = require('http');
 
-function onRequest(req, res) {
-    console.log('request received.');
-    res.writeHead(200, { 'Content-Type': 'text/plan' });
-    res.write('Hello World!');
-    res.end();
+function start() {
+    function onRequest(req, res) {
+        console.log('Request received.');
+        res.writeHead(200, { 'Content-Type': 'text/plan' });
+        res.write('Hello World!');
+        res.end();
+    }
+
+    http.createServer(onRequest).listen(8888);
+    console.log('Server has started');
 }
 
-http.createServer(onRequest).listen(8888);
-
-console.log('Server has started');
+exports.start = start;
