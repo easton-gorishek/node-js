bc3dbf372a4a2fdd9f021bb74c207e034dca6ad8
Easton Gorishek <ieaston26@gmail.com>
2018-07-08 15:54:44 -0700

    added url path

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
-// const http = require('http');
-
-// http.createServer(function(request, response) {
-//     response.writeHead(200, { 'Content-Type': 'text/plain' });
-//     response.write('Hello World!');
-//     response.end();
-// }).listen(8888);
-
 const http = require('http');
+const url = require('url');
 
 function start() {
     function onRequest(req, res) {
-        console.log('Request received.');
+        const pathname = url.parse(req.url).pathname;
+        console.log('Request for ' + pathname + ' received.');
         res.writeHead(200, { 'Content-Type': 'text/plan' });
         res.write('Hello World!');
         res.end();
