5f21e235087f5aedb736aed6d444ba2e392bb139
Easton Gorishek <ieaston26@gmail.com>
2018-07-09 10:02:52 -0700

    brought in exec module

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,13 +1,14 @@
+const exec = require('child_process').exec;
+
 function start() {
     console.log('Request handler "start" was called.');
+    let content = 'empty';
 
-    function sleep(milliSeconds) {
-        const startTime = new Date().getTime();
-        while(new Date().getTime() < startTime + milliSeconds);
-    }
+    exec('ls -lah', function(error, stdout, stderr) {
+        content = stdout;
+    });
 
-    sleep(10000);
-    return 'Hello Start';
+    return content;
 
 }
 
